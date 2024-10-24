8ebbfb2bfa3fb12b25ab2214bfd27e1226527151
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-10-24 18:37:15 +0300

    implemented lazy loading to optimize the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StartingScreen from "./pages/StartingScreen";
-import QuizGame from "./pages/QuizGame";
-import QuizScore from "./pages/QuizScore";
-import PageNotFound from "./pages/PageNotFound";
+import { lazy, Suspense } from "react";
 import { FormProvider } from "./context/FormProvider.jsx";
 import { QuestionsProvider } from "./context/QuestionsProvider.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+
+const StartingScreen = lazy(() => import("./pages/StartingScreen.jsx"));
+const QuizGame = lazy(() => import("./pages/QuizGame.jsx"));
+const QuizScore = lazy(() => import("./pages/QuizScore.jsx"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
 
 export default function App() {
   return (
     <div className="appContainer">
       <BrowserRouter>
-        <FormProvider>
-          <Routes>
-            <Route path="/" element={<StartingScreen />} />
-            <Route
-              path="quizgame"
-              element={
-                <QuestionsProvider>
-                  <QuizGame />
-                </QuestionsProvider>
-              }
-            />
-            <Route
-              path="score"
-              element={
-                <QuestionsProvider>
-                  <QuizScore />
-                </QuestionsProvider>
-              }
-            />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </FormProvider>
+        <Suspense fallback={<Loader />}>
+          <FormProvider>
+            <Routes>
+              <Route path="/" element={<StartingScreen />} />
+              <Route
+                path="quizgame"
+                element={
+                  <QuestionsProvider>
+                    <QuizGame />
+                  </QuestionsProvider>
+                }
+              />
+              <Route
+                path="score"
+                element={
+                  <QuestionsProvider>
+                    <QuizScore />
+                  </QuestionsProvider>
+                }
+              />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </FormProvider>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
